49cc3ac363afa8dcfa5e431da5e0dadb490d6b7b
Kevin Palatzky <github@palatzky.net>
2022-10-26 22:20:06 +0200

    Removed actions

--- src/main/runner/install_old.sh
+++ src/main/runner/install_old.sh
@@ -3,6 +3,15 @@ MICROVAULT_INSTALL_MODE=native
 MICROVAULT_INSTALL_LOCATION=$HOME/.local/microvault
 MICROVAULT_INSTALL_SOURCE=https://github.com/kpalatzky/microvault/releases/latest/download
 
+cat <<'EOF' | cat
+    __  ____               _    __            ____
+   /  |/  (_)_____________| |  / /___ ___  __/ / /_
+  / /|_/ / / ___/ ___/ __ \ | / / __ `/ / / / / __/
+ / /  / / / /__/ /  / /_/ / |/ / /_/ / /_/ / / /_
+/_/  /_/_/\___/_/   \____/|___/\__,_/\__,_/_/\__/
+
+EOF
+
 mkdir -p "$MICROVAULT_INSTALL_LOCATION"
 wget -P "$MICROVAULT_INSTALL_LOCATION $MICROVAULT_INSTALL_SOURCE/microvault"
 chomod +x "$MICROVAULT_INSTALL_LOCATION/microvault"
@@ -11,14 +20,18 @@ ln -s  "$MICROVAULT_INSTALL_LOCATION/microvault" "$MICROVAULT_INSTALL_LOCATION/m
 PATH_VARIABLE='$PATH'
 if [ -f "$HOME/.bashrc" ]; then
 cat <<EOT >> "$HOME/.bashrc"
-# Add MicroVault to Path
+
+# Add MicroVault to path
 export PATH=$MICROVAULT_INSTALL_LOCATION:$PATH_VARIABLE
 EOT
 fi
 
 if [ -f "$HOME/.zshrc" ]; then
 cat <<EOT >> "$HOME/.zshrc"
-# Add MicroVault to Path
+
+# Add MicroVault to path
 export PATH=$MICROVAULT_INSTALL_LOCATION:$PATH_VARIABLE
 EOT
 fi
+
+echo "=> Close and reopen your terminal to start using microvault or run the following to use it now:"
